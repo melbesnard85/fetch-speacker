662646378e1d7bc9577987586b3cf215c369f774
melbesnard85 <melbesnard85@gmail.com>
2022-12-18 02:17:26 +0300

    feat: default routing architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { DetailModule } from './detail/detail.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    DetailModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/detail/detail.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DetailComponent } from './detail.component';
+
+@NgModule({
+  declarations: [
+    DetailComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DetailComponent
+  ]
+})
+export class DetailModule { }
