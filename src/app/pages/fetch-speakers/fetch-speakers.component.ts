ef2d2ac9539cc93a0b8d51ce4d170da5c8367092
melbesnard85 <melbesnard85@gmail.com>
2022-12-19 06:05:02 +0300

    feat: link with search-var to fetch-speacker

--- src/app/pages/fetch-speakers/fetch-speakers.component.ts
+++ src/app/pages/fetch-speakers/fetch-speakers.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fetch-speakers',
   templateUrl: './fetch-speakers.component.html',
   styleUrls: ['./fetch-speakers.component.scss']
 })
-export class FetchSpeakersComponent {
-
+export class FetchSpeakersComponent implements OnInit{
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
+  search(keyword: string) {
+    console.log(keyword)
+  }
 }
--- src/app/pages/fetch-speakers/fetch-speakers.module.ts
+++ src/app/pages/fetch-speakers/fetch-speakers.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 import { FetchSpeakersComponent } from './fetch-speakers.component';
 import { LayoutModule } from 'src/app/layout/layout.module';
 import { FetchSpeakersRoutingModule } from './fetch-speakers-routing.module';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    FetchSpeakersComponent
+    FetchSpeakersComponent,
+    SearchBarComponent
   ],
   imports: [
     CommonModule,
     LayoutModule,
-    FetchSpeakersRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    LayoutModule,
+    FetchSpeakersRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ]
 })
 
