28e8d868e187eb84f04063476868fb863c31015a
melbesnard85 <melbesnard85@gmail.com>
2022-12-19 03:53:08 +0300

    feat: define speaker model and normal http response model

---
+++ src/app/core/models/http/http.model.ts
@@ -0,0 +1,4 @@
+export interface SearchResult<T> {
+  info: T[];
+  result: T[];
+}
---
+++ src/app/core/models/http/speaker.model.ts
@@ -0,0 +1,14 @@
+export interface Speaker<T> {
+    cell: string;
+    dob: T[];
+    email: string;
+    gender: string;
+    id: T[];
+    location: T[];
+    login: T[];
+    name: T[];
+    nat: string;
+    phone: string;
+    picture: string;
+    registered: T[];
+}
